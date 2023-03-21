a7efd5f6a0a90b460192136ea07ca6588f7fbb4a
Ksenia-cha <kseniyachapygova@gmail.com>
2023-03-21 10:56:59 +0300

    add app into package cmd

---
+++ cmd/model.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"context"
+	"os"
+
+	"github.com/Kseniya-cha/LEARN_GOLANG/partitions/pkg/config"
+	"github.com/Kseniya-cha/LEARN_GOLANG/partitions/pkg/database/postgresql"
+	"github.com/Kseniya-cha/LEARN_GOLANG/partitions/pkg/logger"
+	"go.uber.org/zap"
+)
+
+type App interface {
+	Run(context.Context)
+	GracefulShutdown(cancel context.CancelFunc)
+}
+
+// app - прототип приложения
+type app struct {
+	cfg *config.Config
+	log *zap.Logger
+	db  postgresql.IDB
+
+	sigChan chan os.Signal
+}
+
+// NewApp инициализирует прототип приложения
+func NewApp(ctx context.Context, cfg *config.Config) (App, error) {
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	log := logger.NewLogger(cfg)
+
+	db, err := postgresql.NewDB(ctx, cfg.Database, log)
+	if err != nil {
+		return nil, err
+	}
+
+	sigChan := make(chan os.Signal, 1)
+
+	return &app{
+		cfg: cfg,
+		db:  db,
+		log: log,
+
+		sigChan: sigChan,
+	}, nil
+}
